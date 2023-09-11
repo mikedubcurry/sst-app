c3d060d5c1113fef6d8f3b507576bb826cdacb34
mikedubcurry <michaelcurry95@gmail.com>
2023-09-10 21:38:56 -0400

    storage stack

--- packages/functions/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from "sst/node/api";
-
-export const handler = ApiHandler(async (_evt) => {
-  return {
-    statusCode: 200,
-    body: `добрый день. woot! The time is ${new Date().toISOString()}`,
-  };
-});
