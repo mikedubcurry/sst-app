e05595e855b99e5fd2fa9c1d951f07fdcf7cf3c3
mikedubcurry <michaelcurry95@gmail.com>
2023-09-13 15:06:01 -0400

    cors baby

--- packages/core/src/handler.ts
+++ packages/core/src/handler.ts
@@ -19,7 +19,11 @@ export default function handler(
 
         return {
             body,
-            statusCode
+            statusCode,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true
+            }
         }
     }
 }
